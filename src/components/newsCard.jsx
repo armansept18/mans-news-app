313df6fda8ec41776fd23ee71e4e6967674bc0f3
Arman Septian Pratama <armansept18@gmail.com>
2023-10-31 08:16:16 +0700

    feat: add pagination and search bar

--- src/components/newsCard.jsx
+++ src/components/newsCard.jsx
@@ -21,17 +21,26 @@ export const NewsCard = ({ news }) => {
     timeZone: "Asia/Jakarta",
   });
   return (
-    <Card maxW="sm">
-      <CardBody>
+    <Card maxW="sm" style={{ maxHeight: "750px" }}>
+      <CardBody className="flex flex-col items-center">
         {news.urlToImage && (
-          <Image src={news.urlToImage} alt="News" borderRadius="lg" />
+          <Image
+            src={news.urlToImage}
+            alt="News"
+            borderRadius="lg"
+            maxW="250px"
+            maxH="150px"
+            height="100vh"
+          />
         )}
-        <Stack mt="6" spacing="3">
+        <Stack mt="6" spacing="3" style={{ width: "300px", height: "300px" }}>
           <Heading size="md">{news.title}</Heading>
           <Text fontWeight="200">
             {news.author} - {formattedDate}
           </Text>
-          {news.description && <Text>{news.description}</Text>}
+          {news.description && (
+            <Text className="text-ellipsis">{news.description}</Text>
+          )}
         </Stack>
       </CardBody>
       <CardFooter>
