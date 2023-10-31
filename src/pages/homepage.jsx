f9d8d0ee1b76f921cdc39adcceb96da2ae6dd332
Arman Septian Pratama <armansept18@gmail.com>
2023-10-31 16:41:12 +0700

    fix: searchTerm

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -12,19 +12,21 @@ export const Homepage = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
 
-  const fetchNews = async (page = 1, searchTerm = "us") => {
+  const fetchNews = async (page = 1, searchTerm = "") => {
     const pageSize = 4;
     try {
-      const res = await axios.get(
-        `https://newsapi.org/v2/top-headlines?q=${searchTerm}&sortBy=publishedAt&page=${page}&pageSize=${pageSize}`,
-        {
-          headers: {
-            "x-api-key": "7dfc11622365402d85a99e0c3b92f379",
-          },
-        }
-      );
+      const url = searchTerm
+        ? `https://newsapi.org/v2/everything?q=${searchTerm}&sortBy=publishedAt&page=${page}&pageSize=${pageSize}`
+        : `https://newsapi.org/v2/top-headlines?country=id&page=${page}&pageSize=${pageSize}`;
+
+      const res = await axios.get(url, {
+        headers: {
+          "x-api-key": "7dfc11622365402d85a99e0c3b92f379",
+        },
+      });
+
       setNews([...res.data.articles]);
-      setTotalPages(Math.ceil(res.data.totalResults / 5));
+      setTotalPages(Math.ceil(res.data.totalResults / pageSize));
       setCurrentPage(page);
     } catch (err) {
       console.error(err.message);
@@ -34,7 +36,7 @@ export const Homepage = () => {
   };
   const handleSearch = (searchTerm) => {
     setIsLoading(true);
-    fetchNews(searchTerm);
+    fetchNews(1, searchTerm);
     setTimeout(() => {
       setIsLoading(false);
     }, 3000);
